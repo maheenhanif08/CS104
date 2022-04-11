2e7e66645834a826723324db3e6534586865703a
maheenhanif08 <98927148+maheenhanif08@users.noreply.github.com>
2022-04-10 21:43:07 -0400

    Add files via upload

---
+++ Python Queue.py
@@ -0,0 +1,34 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/mahee/Downloads/Monmouth/CS 104 - 01/Python Queue Simulation.py
+Enter Name: Max
+Enter Name: Chris
+Enter Name: Ryan
+Enter Name: Eren
+Enter Name: Levi
+Enter Name: Kylie
+Enter Name: Kendall
+Enter Name: Chloe
+Enter Name: Alisha
+Enter Name: Gina
+Max
+['Chris', 'Ryan', 'Eren', 'Levi', 'Kylie', 'Kendall', 'Chloe', 'Alisha', 'Gina']
+Chris
+['Ryan', 'Eren', 'Levi', 'Kylie', 'Kendall', 'Chloe', 'Alisha', 'Gina']
+Ryan
+['Eren', 'Levi', 'Kylie', 'Kendall', 'Chloe', 'Alisha', 'Gina']
+Eren
+['Levi', 'Kylie', 'Kendall', 'Chloe', 'Alisha', 'Gina']
+Levi
+['Kylie', 'Kendall', 'Chloe', 'Alisha', 'Gina']
+Kylie
+['Kendall', 'Chloe', 'Alisha', 'Gina']
+Kendall
+['Chloe', 'Alisha', 'Gina']
+Chloe
+['Alisha', 'Gina']
+Alisha
+['Gina']
+Gina
+[]
